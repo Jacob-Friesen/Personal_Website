aabb143c5dbcf6ab15ee72703029d8f7041a850a
Jacob-Friesen <F.Jacob.J@gmail.com>
2012-06-28 21:42:00 -0500

    initial commit to Github

---
+++ public/constants.js
@@ -0,0 +1,27 @@
+if (!module) var module = {}
+module.exports = {
+    pages: [
+        "Home",
+        "Skills",
+        "Experience",
+        "Demos",
+        "Blog"
+    ],
+    
+    page_text: {
+        home: {
+            title: "Jacob Friesen - Web Developer",
+            introduction: "Hello, I'm Jacob Friesen a Computer Science Honours Co-op student from the University of Manitoba. \
+                           Currently I'm in a work term. I specialize in scripting languages and web development."
+        },
+        
+        demos: {
+            title: "Demos - Jacob Friesen"
+        },
+        
+        skills: {
+            title: "Skills - Jacob Friesen"
+        } 
+    }
+}
+var constant = module.exports;// make it easy to access client side
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+module.exports = function(app){
+  app.get('/', render_main_page);
+  app.get('/home', render_main_page);
+  app.get('/demos', render_main_page);
+  
+  app.post('/home', function(req, res){
+    res.render('home/index', { title: constant.title, prev_loaded: true });
+  });
+  app.post('/demos', function(req, res){
+    res.render('demos/index', { title: constant.title, prev_loaded: true });
+  });
+}
+
+function render_main_page(req, res){
+  // Whatever page is passed in the system loads
+  if (req.url == "/") req.url = "/" + constant.pages[0].toLowerCase();
+  constant.to_load = req.url.substring(1);//remove /
+  
+  res.render('index', { title: constant.page_text[constant.to_load]["title"], prev_loaded: false});
+}
